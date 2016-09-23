01ae5803c8260ab7160c7a0941daff0c96b0586e
eltonalves <eltim.alves@gmail.com>
2016-09-24 01:10:31 +0200

    json adjusts

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ for house in houses:
 	if title:	
 		i+=1
 		if i > 3:
-			detailList.append(title[0].text)
+			detailList.append({'Title':title[0].text})
 			
 			if len(tbody) > 1:
 				tds = tbody[1].findAll("td")
@@ -37,8 +37,37 @@ for house in houses:
 			
 			for td in tds:
 				if row % 2 == 0:
-					detailList.append(td.text)
-				
+					#detailList.append(td.text)
+					if row == 2:
+						detailList.append({'Type': td.text})
+					elif row == 4:
+						detailList.append({'Period': td.text})
+					elif row == 6:
+						detailList.append({'Address': td.text})
+					elif row == 8:
+						detailList.append({'Size': td.text})
+					elif row == 10:
+						detailList.append({'Rooms': td.text})
+					elif row == 12:
+						detailList.append({'Deposit': td.text})
+					elif row == 14:
+						detailList.append({'MontlyRent': td.text})
+					elif row == 16:
+						detailList.append({'PaymentOnAccount': td.text})
+					elif row == 18:
+						detailList.append({'Furnished': td.text})	
+					elif row == 20:
+						detailList.append({'Smoking': td.text})	
+					elif row == 22:
+						detailList.append({'Tenants': td.text})	
+					elif row == 24:
+						detailList.append({'AdditionalDescription': td.text})	
+					elif row == 26:
+						detailList.append({'Name': td.text})	
+					elif row == 28:
+						detailList.append({'Phone': td.text})	
+					elif row == 30:
+						detailList.append({'Email': td.text})		
 				if not td.text == "Contact information":
 					row+=1
 					
@@ -51,5 +80,5 @@ for house in houses:
 			imgList = list()
 
 
-r = requests.put('https://aarhus-housing.firebaseio.com/aarhus-housing.json',data=str(json.dumps(houseList)))
+r = requests.put('https://aarhus-housing-5b057.firebaseio.com//aarhus-housing.json',data=str(json.dumps(houseList)))
 print r
--- teste.py
+++ teste.py
@@ -2,23 +2,27 @@
 #r = requests.put('https://aarhus-housing.firebaseio.com/teste.json',data='{"arrived":1}')
 
 #print r
+#class Person(object):
+##    def __init__(self, name, profession):
+#        self.name = name
+##        self.profession = profession
+## 
+#people = [Person("Nick", "Programmer"), Person("Alice","Engineer")]
+#professions = dict([ (p.name, p.profession) for p in people ])
+#print professions
+#{"Nick": "Programmer", "Alice": "Engineer"}
+
+#rikke toft noergaard 
+#har du det godt?
+#Jeg har det godt
+#Jeg har det ikke godt
 
 
-lista    = ["titulo","address","value","size"]
-listaa   = ["titulo","address","value","size"]
-listaaa  = ["titulo","address","value","size"]
-listaaaa = ["titulo","address","value","size"]
+#learn how to say, why do you like games in danish
 
-lista2 =[]
+import json
+response = []
 
+response.append({'NOME': 'Elton'})
 
-lista2.append(lista)
-lista2.append(listaa)
-lista2.append(listaaa)
-lista2.append(listaaaa)
-
-
-print lista2
-
-
-
+print json.dumps(response)
